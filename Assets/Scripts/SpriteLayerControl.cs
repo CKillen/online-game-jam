652d696146def4751b86274e99cf13c6e613064c
Unknown <cl_killen@hotmail.com>
2019-08-25 19:37:05 -0400

    Added animation

--- Assets/Scripts/SpriteLayerControl.cs
+++ Assets/Scripts/SpriteLayerControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionTest : MonoBehaviour
+public class SpriteLayerControl : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
@@ -13,12 +13,14 @@ public class CollisionTest : MonoBehaviour
 
     void LateUpdate()
     {
+        //TODO Add logic for detecting if hero is over pivots to fix glitch issues
         spriteRenderer.sortingOrder = (int)Camera.main.WorldToScreenPoint(spriteRenderer.bounds.min).y * -1;
-    }
+    }   
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log((int)Camera.main.WorldToScreenPoint(spriteRenderer.bounds.min).y);
+        
     }
+
 }
---
+++ Assets/Scripts/inanimateObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inanimateObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector3 firstRotation = new Vector3(10f, 0f, 10f);
+        Vector3 secondRotation = new Vector3(10f, 0f, 10f);
+        Vector3 thirdRotation = new Vector3(10f, 0f, 10f);
+        Vector3 fourthRotation = new Vector3(10f, 0f, 10f);
+        gameObject.transform.Rotate(firstRotation * (.5f * Time.deltaTime), Space.Self);
+        gameObject.transform.Rotate(secondRotation * (.5f * Time.deltaTime), Space.Self);
+
+        Debug.Log(gameObject.name);
+    }
+
+}
