7357110f10e4ce0f5e7eacd34314648f0673f0cd
Daniel Bedggood <ddbedggood@gmail.com>
2019-08-25 16:17:57 +1200

    Implement really bad movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            rb2d.velocity = new Vector3(0, 3, 0); 
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            rb2d.velocity = new Vector3(-3, 0, 0);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            rb2d.velocity = new Vector3(0, -3, 0);
+
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            rb2d.velocity = new Vector3(3, 0, 0);
+
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+
+    }
+}
