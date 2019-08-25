ed033da82cccbb203bdb255b7d0269e7204c5061
Unknown <cl_killen@hotmail.com>
2019-08-25 07:27:11 -0400

    Added Movement & Sprites
    Added player Movements. Added New Sprites. Added Movement, collision detection and dynamic sprite layers.

---
+++ Assets/CollisionTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        spriteRenderer.sortingOrder = (int)Camera.main.WorldToScreenPoint(spriteRenderer.bounds.min).y * -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log((int)Camera.main.WorldToScreenPoint(spriteRenderer.bounds.min).y);
+    }
+}
