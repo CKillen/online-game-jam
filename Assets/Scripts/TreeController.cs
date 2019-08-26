9791a5b3fc5a1fd24f8980f2b2aaa69ecb040cfb
Unknown <cl_killen@hotmail.com>
2019-08-25 20:38:37 -0400

    Added bush animation, cleaned up files
    Added bush animations, deleted art that is no longer being used in project

---
+++ Assets/Scripts/TreeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    private Animator shake;
+    private bool shaking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shake = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        shake.Play("treeBump");
+    }
+
+}
